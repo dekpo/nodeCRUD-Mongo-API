d40cc76b3aa4f0270e7268a477aeac6bb77e42ed
Dekpo Yologaza <dekpo@me.com>
2021-03-19 14:38:00 +0100

    Added some controllers and routes for the CRUD

--- src/controllers/crmControllers.js
+++ src/controllers/crmControllers.js
@@ -4,12 +4,48 @@ const { ContactSchema } = require('../models/crmModels');
 // création du model de Contact
 const Contact = mongoose.model('Contact',ContactSchema);
 
-// export de la methode d'ajout de Contact
+// export de la methode d'ajout de contact
 exports.addNewContact = (req,res) => {
     const newContact = new Contact( req.body );
     newContact.save( (err,data) => {
         if (err) res.send(err);
         res.json(data);
         console.log('Method addNewContact from crmControllers: ', data);
+    });
+}
+
+// export de la methode d'affichage de tous les contacts
+exports.getContacts = (req,res) => {
+    Contact.find({},(err,data) => {
+        if (err) res.send(err);
+        res.json(data);
+        console.log('Method getContacts from crmControllers: ', data);
+    });
+}
+
+// export de la methode d'affichage d'un contact par son id
+exports.getContactByID = (req,res) => {
+    Contact.find({ _id:req.params.id },(err,data) => {
+        if (err) res.send(err);
+        res.json(data);
+        console.log('Method getContactByID from crmControllers: ', data);
+    });
+}
+
+// export de la methode de modif d'un contact par son id
+exports.updateContact = (req,res) => {
+    Contact.findByIdAndUpdate( req.params.id, req.body,  {new:true } ,(err,data) => {
+        if (err) res.send(err);
+        res.json(data);
+        console.log('Method updateContact from crmControllers: ', data);
+    });
+}
+
+// export de la methode de suppression d'un contact par son id
+exports.deleteContact = (req,res) => {
+    Contact.deleteOne( { _id:req.params.id }, (err,data) => {
+        if (err) res.send(err);
+        res.json(data);
+        console.log('Method deleteContact from crmControllers: ', data);
     });
 }
--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -9,20 +9,15 @@ const routes = (app) => {
         console.log('Request GET of a user from ' + req.originalUrl );
         next();
 
-    }, (req,res) => {
-        res.send('GET method /contacts WORKS !');
-    })
+    }, controllers.getContacts )
     // la méthode POST me permet d'ajouter un nouveau contact
     .post( controllers.addNewContact );
 
     // route contacts avec param id pour modifier ou supprimer des contacts
     app.route('/contacts/:id')
-    .put( (req,res) => {
-        res.send('PUT method /contacts/' + req.params.id + ' WORKS !')
-    })
-    .delete( (req,res) => {
-        res.send('DELETE method /contacts/' + req.params.id + ' WORKS !')
-    });
+    .get( controllers.getContactByID )
+    .put( controllers.updateContact )
+    .delete( controllers.deleteContact );
 
 }
 
